3eb551ab7f9cd5321f85f8ce199528f164f1ccae
Kendall Felder <61666382+oldgraybuzzard@users.noreply.github.com>
2021-12-21 07:36:06 -0500

    webpack installed and functioning

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,17 @@
-const webpack = require("webpack");
+const webpack = require("webpack")
 const path = require("path");
 
-require("bootstrap");
-
 module.exports = {
-  entry: './assets/js/script.js',
+  entry: "./assets/js/script.js",
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'main.bundle.js'
+    path: path.join(__dirname + "/dist"),
+    filename: "main.bundle.js"
   },
   plugins: [
-    new webpack.ProvidePlugin({
-      $: "jquery",
-      jQuery: "jquery"
-    }),
+  new webpack.ProvidePlugin({
+    $: "jquery",
+    jQuery: "jquery"
+  }),
   ],
-  mode: 'development'
+  mode: "development"
 };
